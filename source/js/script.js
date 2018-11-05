052900a1599c0243af2eff4514f4d540ed3adb0e
Nezdara <nezdara@gmail.com>
2018-11-06 00:07:47 +0200

    added sliders

--- source/js/script.js
+++ source/js/script.js
@@ -1,105 +1,230 @@
-// Открытие и закрыте Меню
+// window.addEventListener("keydown", function (evt) {
+//   if (evt.keyCode === 27) {
+//     if (modalForm.classList.contains("modal-show")) {
+//       evt.preventDefault();
+//       modalForm.classList.remove("modal-show");
+//       overlay.classList.remove("modal-show");
+//     }
+//     if (modalFormDone.classList.contains("modal-show")) {
+//       evt.preventDefault();
+//       modalFormDone.classList.remove("modal-show");
+//       overlay.classList.remove("modal-show");
+//     }
+//     if (modalRecall.classList.contains("modal-show")) {
+//       evt.preventDefault();
+//       modalRecall.classList.remove("modal-show");
+//       overlay.classList.remove("modal-show");
+//     }
+//   }
+// });
 
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
-var service = document.querySelector('.main-nav__service');
-
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-  }
-});
-
-
-// Открытие и закрытие Модальных окон.
-
-var promo__btn = document.querySelector(".promo__btn");
-var recall__btn = document.querySelector(".recall__btn");
-var product__callBtn = document.querySelector(".product__call-btn");
-var aboutUs__btn = document.querySelector(".about-us__btn");
-var example__btn = document.querySelector(".about-us__btn");
-var example__btn = document.querySelector(".example__btn");
-var contact__btn = document.querySelector(".contact__btn");
-
-var modalForm = document.querySelector(".modal-form");
-var modalFormDone = document.querySelector(".modal-form-done");
-var modalRecall = document.querySelector(".modal-recall");
-var overlay = document.querySelector(".modal-overlay");
-
-var modalFormClose = document.querySelector(".modal-form .close-btn");
-var modalFormDoneClose = document.querySelector(".modal-form-done .close-btn");
-var modalRecallClose = document.querySelector(".modal-recall .close-btn");
-
-if (promo__btn) {
-  promo__btn.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    modalForm.classList.add("modal-show");
-    overlay.classList.add("modal-show");
+$(document).ready(function() {
+  $('.main-nav__toggle').on('click', function(e) {
+    e.preventDefault();
+    $('.main-nav').toggleClass('main-nav--closed');
+    $('.main-nav').toggleClass('main-nav--opened');
   });
-}
 
-if (recall__btn) {
-  recall__btn.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    modalFormDone.classList.add("modal-show");
-    overlay.classList.add("modal-show");
+  $('.promo__btn, .product__call-btn, .example__btn, .main-nav__service-call').on('click', function(e) {
+    e.preventDefault();
+    $(".modal-form, .modal-overlay").addClass("modal-show");
   });
-}
 
-if (product__callBtn) {
-  product__callBtn.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    modalForm.classList.add("modal-show");
-    overlay.classList.add("modal-show");
+  $('.contact__btn, .about-us__btn').on('click', function(e) {
+    e.preventDefault();
+    $(".modal-recall, .modal-overlay").addClass("modal-show");
   });
-}
 
-if (example__btn) {
-  example__btn.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    modalForm.classList.add("modal-show");
-    overlay.classList.add("modal-show");
+  $('.recall__btn').on('click', function(e) {
+    e.preventDefault();
+    $(".modal-form-done, .modal-overlay").addClass("modal-show");
   });
-}
 
-modalFormDoneClose.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  modalFormDone.classList.remove("modal-show");
-  overlay.classList.remove("modal-show");
-});
+  $('.close-btn').on('click', function(e) {
+    e.preventDefault();
+    $(".modal-form-done, .modal-recall, .modal-form, .modal-overlay").removeClass("modal-show");
+  });
 
-modalFormClose.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  modalForm.classList.remove("modal-show");
-  overlay.classList.remove("modal-show");
-});
+  $('.js-btn-1').on('click', function(e) {
+    e.preventDefault();
+    $(".product__brand-link").removeClass("product__brand-link--curent");
+    $(this).addClass("product__brand-link--curent");
+    $(".product__type-link").removeClass("product__type-link--curent");
+    $(".js-btn-3").addClass("product__type-link--curent");
+    $(".product__slider").hide();
+    $(".product__slider1").show();
+    $('.product__list').slick('destroy');
+    $('.js-slider-1').slick('reinit');
+  });
 
-modalRecallClose.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  modalRecall.classList.remove("modal-show");
-  overlay.classList.remove("modal-show");
-});
+  $('.js-btn-2').on('click', function(e) {
+    e.preventDefault();
+    $(".product__brand-link").removeClass("product__brand-link--curent");
+    $(this).addClass("product__brand-link--curent");
+    $(".product__type-link").removeClass("product__type-link--curent");
+    $(".js-btn-3").addClass("product__type-link--curent");
+    $(".product__slider").hide();
+    $(".product__slider3").show();
+    $('.product__list').slick('destroy');
+    $('.js-slider-3').slick('reinit');
+  });
 
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    if (modalForm.classList.contains("modal-show")) {
-      evt.preventDefault();
-      modalForm.classList.remove("modal-show");
-      overlay.classList.remove("modal-show");
+  $('.js-btn-3').on('click', function(e) {
+    e.preventDefault();
+    $(".product__type-link").removeClass("product__type-link--curent");
+    $(this).addClass("product__type-link--curent");
+    $(".product__slider").hide();
+    if ($('.js-btn-1').hasClass('product__brand-link--curent')) {
+      $(".product__slider1").show();
+      $('.product__list').slick('destroy');
+      $('.js-slider-1').slick('reinit');
+    } else {
+      $(".product__slider3").show();
+      $('.product__list').slick('destroy');
+      $('.js-slider-3').slick('reinit');
     }
-    if (modalFormDone.classList.contains("modal-show")) {
-      evt.preventDefault();
-      modalFormDone.classList.remove("modal-show");
-      overlay.classList.remove("modal-show");
+  });
+
+  $('.js-btn-4').on('click', function(e) {
+    e.preventDefault();
+    $(".product__type-link").removeClass("product__type-link--curent");
+    $(this).addClass("product__type-link--curent");
+    $(".product__slider").hide();
+    if ($('.js-btn-1').hasClass('product__brand-link--curent')) {
+      $(".product__slider2").show();
+      $('.product__list').slick('destroy');
+      $('.js-slider-2').slick('reinit');
+    } else {
+      $(".product__slider4").show();
+      $('.product__list').slick('destroy');
+      $('.js-slider-4').slick('reinit');
     }
-    if (modalRecall.classList.contains("modal-show")) {
-      evt.preventDefault();
-      modalRecall.classList.remove("modal-show");
-      overlay.classList.remove("modal-show");
-    }
-  }
+  });
+
+  $('.example__list').slick();
+
+  $('.js-slider-1').slick({
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    infinite: false,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          arrows: true
+        }
+      },
+      {
+        breakpoint: 850,
+        settings: {
+          slidesToShow: 2,
+          arrows: true
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          arrows: true
+        }
+      }
+    ]
+  });
+
+  $('.js-slider-2').slick({
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    infinite: false,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          arrows: true
+        }
+      },
+      {
+        breakpoint: 850,
+        settings: {
+          slidesToShow: 2,
+          arrows: true
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          arrows: true
+        }
+      }
+    ]
+  });
+
+  $('.js-slider-3').slick({
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    infinite: false,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          arrows: true
+        }
+      },
+      {
+        breakpoint: 850,
+        settings: {
+          slidesToShow: 2,
+          arrows: true
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          arrows: true
+        }
+      }
+    ]
+  });
+
+  $('.js-slider-4').slick({
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    infinite: false,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          arrows: true
+        }
+      },
+      {
+        breakpoint: 850,
+        settings: {
+          slidesToShow: 2,
+          arrows: true
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          arrows: true
+        }
+      }
+    ]
+  });
+
 });
